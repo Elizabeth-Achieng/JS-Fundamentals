219796c5ab39d015ddc6103455bd1fb33a43aa64
Elizabeth Achieng <achiengelizabeth470@gmail.com>
2025-11-09 22:18:21 +0300

    commit

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+const times = parseInt(process.argv[2], 10);
+
+if (isNaN(times) || times <= 0) {
+  console.log("Missing number of occurrences");
+} else {
+  let i = 0;
+  while (i < times) {
+    console.log("C is fun");
+    i++;
+  }
+}
